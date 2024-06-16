7f741a7839a02a15e3456fdf452acd1a1d9476bc
anabiax <anabia200113@gmail.com>
2024-06-15 22:24:15 -0300

    adiciona pag agendamento produto

--- pages/Produtos/AgendamentoProduto.jsx
+++ pages/Produtos/AgendamentoProduto.jsx
@@ -6,7 +6,55 @@ export default function AgendamentoProduto() {
     
     return (
         <>
-            
+            <div className="container">
+            <div className="produto">
+            <img src="https://sintemais.com.br/wp-content/uploads/2017/05/L-_martelado_cinza_escuro_1-2_C07_com_malha.jpg" alt="Nome do Produto" className="imagem-produto" />
+            <div className="info-produto">
+                <h2>Nome do Produto</h2>
+                <p>Descrição do Produto</p>
+                <p>Preço do Produto</p>
+
+            <label htmlFor="date">Escolha a data:</label>
+            <input
+                type="date"
+                id="date"
+                name="date"
+            />
+            <br></br>
+            <br></br>
+
+            <label htmlFor="time">Escolha o horário:</label>
+            <input
+                type="time"
+                id="time"
+                name="time"
+            />
+            <br></br>
+            <br></br>
+
+                <div>
+                            <input type="radio" id="pequenoporte" name="porte" value="medio" />
+                            <label htmlFor="pequenoporte">Pequeno porte</label> 
+                            <br></br>
+                            <input type="radio" id="grandeporte" name="porte" value="grande" />
+                            <label htmlFor="grandeporte">Grande porte</label>
+                            <br></br>
+                            <br></br>
+                </div>
+
+                <div className="simulador-frete">
+                            <div>Simulador de Frete</div>
+                            <div>
+                                <input type="text" placeholder="CEP" />
+                            </div>
+                            <div>R$ 30,00</div>
+                            <br></br>
+                        </div>
+
+                <button className="botao-produtos">Adicionar ao Carrinho</button>
+            </div>
+            </div>
+            </div>
         </>
-    )
+    );
 }
--- pages/Servicos/AgendamentoServico.jsx
+++ pages/Servicos/AgendamentoServico.jsx
@@ -1,12 +1,54 @@
 import './AgendamentoServico.css';
+//import { useHistory } from 'react-router-dom';
 
 
 export default function AgendamentoServico() {
+    //const history = useHistory();
 
-    
+    /*const handleAgendarServico = () => {
+        history.push('/');
+    };*/
+
     return (
         <>
-            
+            <div className="container">
+                <div className="servico">
+                    <img src="https://sintemais.com.br/wp-content/uploads/2017/05/L-_martelado_cinza_escuro_1-2_C07_com_malha.jpg" alt="Nome do Serviço" className="imagem-servico" />
+                    <div className="info-servico">
+                        <h2>Nome do Serviço</h2>
+                        <p>Descrição do Serviço</p>
+                        <p>Preço do Serviço</p>
+
+                        <div>
+                            <input type="radio" id="pacoteMedio" name="pacote" value="medio" />
+                            <label htmlFor="pacoteMedio">Pacote Médio</label> 
+                            <br></br>
+                            <input type="radio" id="pacoteGrande" name="pacote" value="grande" />
+                            <label htmlFor="pacoteGrande">Pacote Grande</label>
+                            <br></br>
+                            <br></br>
+                        </div>
+
+                        <div>
+                            <input type="checkbox" id="entregaCasa" name="entregaCasa" />
+                            <label htmlFor="entregaCasa">Desejo que seja entregue em casa</label>
+                            <br></br>
+                            <br></br>
+                        </div>
+
+                        <div className="simulador-frete">
+                            <div>Simulador de Frete</div>
+                            <div>
+                                <input type="text" placeholder="CEP" />
+                            </div>
+                            <div>R$ 30,00</div>
+                            <br></br>
+                        </div>
+
+                        <button className="botao-servicos">Agendar Serviço</button>
+                    </div>
+                </div>
+            </div>
         </>
-    )
+    );
 }
